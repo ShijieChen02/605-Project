a85570139931a595aad2e2631ed31b172ca3a8bd
hzhang987 <147096328+hzhang987@users.noreply.github.com>
2023-12-14 07:55:11 +0800

    Create load_data_2.sh

---
+++ load_data_2.sh
@@ -0,0 +1,16 @@
+File Edit Options Buffers Tools Sh-Script Help
+#!/bin/bash
+
+# untar your R installation. Make sure you are using the right version!
+tar -xzf R413.tar.gz
+# (optional) if you have a set of packages (created in Part 1), untar them also
+tar -xzf packages.tar.gz
+
+# make sure the script will use your R installation,
+# and the working directory as its home location
+export PATH=$PWD/R/bin:$PATH
+export RHOME=$PWD/R
+export R_LIBS=$PWD/packages
+
+# run your script
+Rscript load_data_2.R
